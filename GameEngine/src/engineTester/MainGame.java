46acaae936bcbe957ac2084ea0318c08b288f3b6
felix <felix.cumarav@gmail.com>
2024-12-03 22:25:07 +0100

    I almost added texture to rectangle

--- GameEngine/src/engineTester/MainGame.java
+++ GameEngine/src/engineTester/MainGame.java
@@ -1,18 +1,22 @@
 package engineTester;
 
+import models.TexturedModel;
 import org.lwjgl.opengl.Display;
 
 import renderEngine.DisplayManager;
 import renderEngine.Loader;
-import renderEngine.RawModel;
+import models.RawModel;
 import renderEngine.Renderer;
 import shaders.StaticShader;
+import textures.ModelTexture;
+
+import java.io.IOException;
 
 
 public class MainGame {
 
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         DisplayManager.createDisplay();
         Loader loader = new Loader();
@@ -30,14 +34,22 @@ public class MainGame {
                 0,1,3,//top left triangle (v0, v1, v3)
                 3,1,2//bottom right triangle (v3, v1, v2)
         };
+        float[] textureCoords={
+          0,0, //v0
+          0,1, //v1
+          1,1, //v2
+          1,0  //v3
+        };
 
-        RawModel model = loader.loadToVAO(vertices, indices);
+        RawModel model = loader.loadToVAO(vertices, textureCoords,indices);
+        ModelTexture texture = new ModelTexture(loader.loadTexture("texture01"));
+        TexturedModel texturedModel = new TexturedModel(model,texture);
 
         while (!Display.isCloseRequested()) {
             // game logic
             renderer.prepare();
             shader.start();
-            renderer.render(model);
+            renderer.render(texturedModel);
             shader.stop();
             DisplayManager.updateDisplay();
         }
