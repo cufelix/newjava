6720a42693f61c6e7a29d952e15fa9de4cd38517
felix <felix.cumarav@gmail.com>
2024-12-21 01:41:33 +0100

    I added terrain generation

--- GameEngine/src/engineTester/MainGame.java
+++ GameEngine/src/engineTester/MainGame.java
@@ -12,8 +12,9 @@ import renderEngine.DisplayManager;
 import renderEngine.Loader;
 import renderEngine.MasterRender;
 import renderEngine.OBJLoader;
-import renderEngine.Renderer;
+import renderEngine.EntityRenderer;
 import shaders.StaticShader;
+import terains.Terain;
 import textures.ModelTexture;
 
 import java.io.FileNotFoundException;
@@ -31,25 +32,31 @@ public class MainGame {
 
         RawModel model = null;
         try {
-            model = OBJLoader.loadObjModel("map",loader);
+            model = OBJLoader.loadObjModel("stall",loader);
         } catch (FileNotFoundException e) {
             throw new RuntimeException(e);
         }
 
-        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("file")));
+        TexturedModel staticModel = new TexturedModel(model, new ModelTexture(loader.loadTexture("stallTexture")));
         ModelTexture texture = staticModel.getTexture();
         texture.setShineDamper(10);
         texture.setReflectivity(0.4f);
-        Vector3f vecpo = new Vector3f(0,20,20);
+        Vector3f vecpo = new Vector3f(6000,4000,4000);
         Vector3f vecco = new Vector3f(1,1,1);
 
-        Entity entity = new Entity(staticModel, new Vector3f(0, 0.5f, -2), 40, 0, 0, 1);
+        Entity entity = new Entity(staticModel, new Vector3f(50, 0, 15), 0, 0, 0, 1);
         Light light = new Light(vecco, vecpo);
+
+        Terain terrain1= new Terain(0,0,loader,new ModelTexture(loader.loadTexture("grass")));
+        Terain terrain2= new Terain(1,0,loader,new ModelTexture(loader.loadTexture("grass")));
+
         Camera camera = new Camera();
         MasterRender Mrenderer = new MasterRender();
         while (!Display.isCloseRequested()) {
-            entity.increaseRotation(0, -0.25f, 0);
+            entity.increaseRotation(0, 1, 0);
             camera.move();
+            Mrenderer.processTerrain(terrain1);
+            Mrenderer.processTerrain(terrain2);
             Mrenderer.processEntity(entity);
             Mrenderer.render(light,camera);
             DisplayManager.updateDisplay();
--- GameEngine/src/entities/Camera.java
+++ GameEngine/src/entities/Camera.java
@@ -2,12 +2,15 @@ package entities;
 
 
 import org.lwjgl.input.Keyboard;
+import org.lwjgl.input.Mouse;
 import org.lwjgl.util.vector.Vector3f;
+
+import java.awt.event.MouseEvent;
 
 
 public class Camera {
 
-    private final Vector3f position = new Vector3f(0, 0, 0);
+    private final Vector3f position = new Vector3f(25, 2, 50);
     private float pitch;
     private float yaw;
     private float roll;
@@ -17,14 +20,24 @@ public class Camera {
 
     public void move() {
         if (Keyboard.isKeyDown(Keyboard.KEY_W)) {
-            position.z -= 0.02f;
+            position.z -= 0.05f;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_D)) {
-            position.x += 0.02f;
+            position.x += 0.04f;
         }
         if (Keyboard.isKeyDown(Keyboard.KEY_A)) {
-            position.x -= 0.02f;
+            position.x -= 0.04f;
         }
+        if (Keyboard.isKeyDown(Keyboard.KEY_S)) {
+            position.z += 0.05f;
+        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_SPACE)) {
+            position.y += 0.04f;
+        }
+        if (Keyboard.isKeyDown(Keyboard.KEY_E)) {
+            position.y -= 0.04f;
+        }
+
     }
 
 
